83890ac7239bd3e1dd0d001b566d9e2a01a1ff6b
Xin Li <usigwt@gmail.com>
2019-01-24 15:58:58 -0800

    Update R_ccmice_qtl.R

--- R_ccmice_qtl.R
+++ R_ccmice_qtl.R
@@ -142,6 +142,12 @@ source(file.path(dir_ccmice, "html.report_Xin.R"))
 html.report_Xin(file.path(dir_ccmice, 'docs', 'QTL'), qtl_corrected[c(1,2)], perms = perm_max[c(1,2),], assoc = FALSE)
 html.report_Xin(file.path(dir_ccmice, 'docs', 'QTL'), qtl_corrected[c(3,4)], perms = perm_max[c(3,4),], assoc = FALSE)
 
+EarSwell_qtl = rbind(qtl$EarSwell$lod$A, qtl$EarSwell$lod$X);
+write.table(EarSwell_qtl, file = file.path(dir_data, "docs", "QTL", "Earswell_pvalue.txt"), append = FALSE, quote = FALSE, sep = "\t",
+            eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
+
 save.image(file=file.path(dir_data, "tempCache", "ccmice_10202019.RData"))
 savehistory("~/mac_hdd/ccmice/tempCache/ccmice_apr16.Rhistory")
 
