854039df848532602de0516d30e00e27bf8b6839
Xin Li <usigwt@gmail.com>
2019-01-27 20:59:03 -0800

    Update parse_qtl.R

--- parse_qtl.R
+++ parse_qtl.R
@@ -3,13 +3,16 @@
 parse_qtl = function( qtl, ccmice_hap ){
 qtl_table = NULL
 for( i in names(qtl) ) {
-	EarSwell_qtl = rbind(qtl[[i]]$lod$A, qtl[[i]]$lod$X);
-	pvalue = EarSwell_qtl[,5:9]
+	EarSwell_lod = rbind(qtl[[i]]$lod$A, qtl[[i]]$lod$X);
+	EarSwell_coef = rbind(qtl[[i]]$coef$A, qtl[[i]]$coef$X);
+	pvalue = EarSwell_lod[,5:9]
+	coef = EarSwell_coef
 	dimnames(pvalue)[[2]] = paste(dimnames(pvalue)[[2]], i, sep="_")
+	dimnames(coef)[[2]] = paste(dimnames(coef)[[2]], i, sep="_")
 	if( is.null(qtl_table) ) {
-		qtl_table = EarSwell_qtl[,1:4]
+		qtl_table = EarSwell_lod[,1:4]
 	}
-	qtl_table = cbind(qtl_table, pvalue[dimnames(qtl_table)[[1]],])
+	qtl_table = cbind(qtl_table, pvalue[dimnames(qtl_table)[[1]],], coef[dimnames(coef)[[1]],])
 }
 qtl_table = cbind(qtl_table, ccmice_hap[dimnames(qtl_table)[[1]],])
 return(qtl_table)
