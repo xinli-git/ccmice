b85346697562be3efaee4afa8e3fd243040ef7eb
Xin Li <usigwt@gmail.com>
2019-08-05 15:45:00 +0800

    Mon Aug  5 15:45:00 CST 2019

---
+++ docs/genotype/extract_genotype.sh
@@ -0,0 +1,4 @@
+
+
+tblmap -s'chromosome==1 && position_b38 > 82200000 && position_b38 < 88500000' -d ',' -k'marker,chromosome,position_b38,CC009/UncM3045,CC027/GeniUncM756,CC037/TauUncM955,CC049/TauUncM4214,CC060/UncM813' ~/mac_hdd/ccmice/genotype/SEQgenotypes.csv
+
